e3d507ca115690ebe89cb0515f457182087a9a5e
Harsh <69311741+harshsinghs1058@users.noreply.github.com>
2021-06-05 13:38:44 +0530

    Add files via upload

---
+++ A_Hexagonal_numbers.cpp
@@ -0,0 +1,38 @@
+//this code is created by HARSH.
+#include <bits/stdc++.h>
+#define f(i, n) for (lli i = 0; i < (n); i++)
+#define fs(i, p, n) for (lli i = (p); i < (n); i++)
+#define fi(i, p, n) for (lli i = (p); i <= (n); i++)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define p(x) cout << x << endl
+#define A(x) x.begin(), x.end()
+#define input(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cin >> x[i];               \
+    }
+#define print(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cout << x[i] << " ";       \
+    }
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long int lli;
+typedef long int li;
+const lli N = 10000015;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    cout.precision(20);
+    lli n;
+    cin >> n;
+    cout << (2 * n * n) - n;
+    return 0;
+}
---
+++ A_Is_it_rated_2.cpp
@@ -0,0 +1,36 @@
+//this code is created by HARSH.
+#include <bits/stdc++.h>
+#define f(i, n) for (lli i = 0; i < (n); i++)
+#define fs(i, p, n) for (lli i = (p); i < (n); i++)
+#define fi(i, p, n) for (lli i = (p); i <= (n); i++)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define p(x) cout << x << endl
+#define A(x) x.begin(), x.end()
+#define input(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cin >> x[i];               \
+    }
+#define print(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cout << x[i] << " ";       \
+    }
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long int lli;
+typedef long int li;
+const lli N = 10000015;
+
+int main()
+{
+    string s;
+    while (getline(cin, s))
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
