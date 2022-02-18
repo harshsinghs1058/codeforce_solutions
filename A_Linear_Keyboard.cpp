60a44bd7e7da3bfa189698e302f5b613af7cc220
harshsinghs1058 <harshsinghs1058@gmail.com>
2022-02-18 10:45:04 +0530

    17-02-2022

---
+++ A_Linear_Keyboard.cpp
@@ -0,0 +1,58 @@
+// this code is created by HARSH.
+#include <bits/stdc++.h>
+#define f(i, n) for (lli i = 0; i < (n); i++)
+#define fs(i, p, n) for (lli i = (p); i < (n); i++)
+#define fi(i, p, n) for (lli i = (p); i <= (n); i++)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIX(x, y) ((x) < (y) ? (x) : (y))
+#define p(x) cout << x << endl
+#define A(x) x.begin(), x.end()
+#define input(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cin >> x[i];               \
+    }
+#define print(x)                   \
+    f(i, sizeof(x) / sizeof(x[0])) \
+    {                              \
+        cout << x[i] << " ";       \
+    }
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long int lli;
+typedef long int li;
+const lli N = 10000015;
+
+void solve()
+{
+    string k,s;
+    cin >> k;
+    cin >> s;
+    int a[26];
+    f(i,26){
+        a[k[i] - 'a'] = i;
+    }
+    int ans = 0;
+    fs(i,1,s.size()){
+        ans += abs(a[s[i] - 'a'] - a[s[i - 1] - 'a']);
+    }
+    cout << ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    cout.precision(20);
+    int t, i = 1;
+    cin >> t;
+    while (t--)
+    {
+        //cout << "Case #" <<i++<<": ";
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+}
